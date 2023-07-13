f0279fb38abec45bca1556c43ffab94e94e5931f
Gonzalo <gonzalomedinamedina@gmail.com>
2023-07-13 22:00:48 +0200

    Created function to filter podcasts by user input.

--- src/app/mainView/mainView.tsx
+++ src/app/mainView/mainView.tsx
@@ -5,9 +5,7 @@ import PodcastIcon from "./podcastIcon";
 
 export default function MainView()
 {
-
     const [podcastSearchInput, setPodcastSearchInput] = useState('');
-    const [numberOfPodcastFound, setNumberOfPodcastFound] = useState(0);
     const [podcasts, setPodcasts] = useState([]);
 
     useEffect(() =>
@@ -19,32 +17,41 @@ export default function MainView()
                 const entry = data!.feed!.entry!
                 if (entry.length! > 0)
                 {
-                    setPodcasts(entry);
+                    const podcastsComponents = entry.map((p) => 
+                    {
+                        return <PodcastIcon podcastInfo={p}></PodcastIcon>
+                    });
+                    
+                    setPodcasts(podcastsComponents);
                 }
             });
     }, []);
-
-    const getPodcasts = () =>
+    
+    const podcastMatchWithSearchInput = (podcast: any) =>
     {
-        return podcasts;
+        if (podcastSearchInput === '')
+            return true;
+            
+        const podcastInfo = podcast!.props!.podcastInfo;
+        const title: string = podcastInfo!.title!.label;
+        const author: string = podcastInfo["im:artist"]!.label;
+    
+        return title.toLowerCase().includes(podcastSearchInput) || 
+            author.toLowerCase().includes(podcastSearchInput);
     }
 
-    const cachedPodcasts: any[] = useMemo(() =>{ return getPodcasts() }, [podcastSearchInput, podcasts]);
-
-    podcasts.forEach((p) => {console.log(p)});
-
-    const podcastComponents = cachedPodcasts.map((p) => {return <PodcastIcon podcastInfo={p}></PodcastIcon>});
+    const cachedPodcasts: any[] = useMemo(() => podcasts.filter((p) => podcastMatchWithSearchInput(p) === true) , [podcasts, podcastSearchInput]);
 
     return(
         <div className="relative flex flex-col py-5 min-w-full">
             <div className="flex justify-end top-10 right-10">
                 <div className="min-w-10 w-10 bg-blue-600 place-content-center rounded-full">
-                    <p className="text-center text-white">{numberOfPodcastFound}</p>
+                    <p className="text-center text-white">{cachedPodcasts.length}</p>
                 </div>
-                <input className="border-2 mx-5 px-2" onChange={(e) => {setPodcastSearchInput(e.target.value)}} placeholder="Filter podcasts..."></input>
+                <input className="border-2 mx-5 px-2" onChange={(e) => {setPodcastSearchInput(e.target.value.toLowerCase())}} placeholder="Filter podcasts..."></input>
             </div>
-            <div className="grid grid-cols-4">
-                {podcastComponents}
+            <div className="grid grid-cols-4 py-5">
+                {cachedPodcasts}
             </div>
         </div>
     )

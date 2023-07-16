a8d66b75decb141d1ef8e682205d2acd67a47cdd
gonzalo <gonzalomedinamedina@gmail.com>
2023-07-16 16:28:36 +0200

    Moving common layout outside of page file.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,6 @@ import MainView from './mainView/mainView'
 export default function Home() 
 {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-start p-5">
-      <div className="self-start min-w-full">
-        <h1 className="text-blue-600 text-3xl">Podcaster</h1>
-        <hr></hr>
-      </div>
       <MainView></MainView>
-    </main>
   )
 }

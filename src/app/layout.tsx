981f12270d02296717cca9313d1ba398b23b4ff6
gonzalo <gonzalomedinamedina@gmail.com>
2023-07-16 16:28:08 +0200

    Changing common root layout and adding one folder level to apply tailwind.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,19 @@ export default function RootLayout({children,}: {children: React.ReactNode})
 {
   return (
     <html lang="en">
-      <body className="bg-white">{children}</body>
+      <body className="bg-white">        
+        <main className="flex min-h-screen flex-col items-center justify-start p-5">
+          <div className="self-start min-w-full">
+            <a href="/">
+              <h1 className="text-blue-600 text-3xl">Podcaster</h1>
+            </a>
+            <hr></hr>
+          </div>
+          <div className="relative flex flex-col py-5 min-w-full">
+            {children}
+          </div>
+        </main>
+      </body>
     </html>
   )
 }

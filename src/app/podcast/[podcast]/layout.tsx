14697db9fe7ebd582c11ca4109b7bde2946452da
Gonzalo <gonzalomedinamedina@gmail.com>
2023-07-16 19:36:14 +0200

    Adding states and setting it's values in useEffect hook. Reading podcast info from localStorage.

--- src/app/podcast/[podcast]/layout.tsx
+++ src/app/podcast/[podcast]/layout.tsx
@@ -4,27 +4,38 @@ import { useEffect, useState, useMemo } from "react"
 
 export default function Layout({params, children,}: {children: React.ReactNode})
 {
+    const [summary, setSummary] = useState('')
+    const [imageSource, setImageSource] = useState('')
+    const [author, setAuthor] = useState('')
+    const [title, setTitle] = useState('')
+
     const podcast = params.podcast;
-    const [podcastInfo, setPodcastInfo] = useState([]);
-  
+    const [podcastDetails, setPodcastDetails] = useState([]);
+    var cachedPodcastInfo: any;
+
     useEffect(() => 
     {
+        const cachedPodcastInfoString = localStorage.getItem(`podcast${podcast}`);
+        cachedPodcastInfo = JSON.parse(cachedPodcastInfoString ? cachedPodcastInfoString : '');
+        setSummary(cachedPodcastInfo.summary.label);
+
         fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(`https://itunes.apple.com/lookup?id=${podcast}`)}`)
         .then(response => response.json())
         .then(data => 
         {
             var jsonContent = JSON.parse(data.contents);
             if (jsonContent!.resultCount === 1)
-                setPodcastInfo(jsonContent.results[0]);                  
+            {
+                setPodcastDetails(jsonContent.results[0]);       
+                setImageSource(cachedPodcastDetails.artworkUrl600);
+                setAuthor(cachedPodcastDetails.artistName);
+                setTitle(cachedPodcastDetails.collectionName?.toUpperCase());           
+            }
         })
         .catch(e => console.error(e))
-
     });
 
-    const cachedPodcast: any = useMemo(() => podcastInfo, [podcastInfo]);
-    const imageSource = cachedPodcast!.artworkUrl600;
-    const author = cachedPodcast?.artistName;
-    const title = cachedPodcast?.collectionName?.toUpperCase();
+    const cachedPodcastDetails: any = useMemo(() => podcastDetails, [podcastDetails]);
 
     return (<div className=" flex flex-row">
         <div className= " justify-center items-center shadow-md border m-5 mt-20 p-5">
@@ -33,7 +44,7 @@ export default function Layout({params, children,}: {children: React.ReactNode})
             <h1 className=" text-center font-semibold">{title}</h1>
             <p  className=" text-center text-gray-400">{"by " + author}</p>
             <hr></hr>
-
+            <div>{summary}</div>
         </div>
         <div>{children}</div>
     </div>)

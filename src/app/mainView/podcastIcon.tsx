7e5b3e147a7d61fe41306a71acb339b05a7847f6
gonzalo <gonzalomedinamedina@gmail.com>
2023-07-16 16:29:58 +0200

    Added <a> element to wrap PodcastIcon component

--- src/app/mainView/podcastIcon.tsx
+++ src/app/mainView/podcastIcon.tsx
@@ -5,13 +5,15 @@ export default function PodcastIcon({podcastInfo})
     const image = podcastInfo["im:image"]![2]!.label;
     const id = podcastInfo!.id!.attributes["im:id"];
 
-    return <div key={id} className=" justify-center items-center shadow-md border m-5 mt-20">
-        <div className="flex justify-center items-center">
-            <img className="relative rounded-full -mt-20" src={image}></img>
+    return <a href={`/podcast/${id}`}>
+        <div key={id} className=" justify-center items-center shadow-md border m-5 mt-20">
+            <div className="flex justify-center items-center">
+                <img className="relative rounded-full -mt-20" src={image}></img>
+            </div>
+            <div>
+                <h1 className=" text-center font-semibold">{title.toUpperCase()}</h1>
+                <p  className=" text-center text-gray-400">{"Author: " + author}</p>
+            </div>
         </div>
-        <div>
-            <h1 className=" text-center font-semibold">{title.toUpperCase()}</h1>
-            <p  className=" text-center text-gray-400">{"Author: " + author}</p>
-        </div>
-    </div>
+    </a>
 }

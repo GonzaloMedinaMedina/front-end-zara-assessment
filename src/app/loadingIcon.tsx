da5421b66ef570a05a47156da0b6071ba9ac1b62
Gonzalo <gonzalomedinamedina@gmail.com>
2023-07-19 01:53:58 +0200

    Added loading icon component.

---
+++ src/app/loadingIcon.tsx
@@ -0,0 +1,5 @@
+export default function LoadingIcon({showIcon})
+{
+    const display = showIcon ? 'block' : 'hidden'
+    return <div className={display + " animate-border bg-blue-700 rounded-full w-10 h-10 border-1 border-blue-300 mb-5"}/>
+}

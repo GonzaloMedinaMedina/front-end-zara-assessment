cbd2ab2a65d0dbe06922d967caec5857658807d0
gonzalo <gonzalomedinamedina@gmail.com>
2023-07-18 20:42:56 +0200

    Reading podcastDetails info from cache. Adding <a> to wrap podcastDetails component. Added podcast description. Wrap child component with podcastDetails context info.

--- src/app/podcast/[podcast]/layout.tsx
+++ src/app/podcast/[podcast]/layout.tsx
@@ -1,51 +1,79 @@
 
 'use client'
-import { useEffect, useState, useMemo } from "react"
+import { useEffect, useState, createContext } from "react"
+import { readCachedData } from "../../utils";
 
-export default function Layout({params, children,}: {children: React.ReactNode})
+export const PodcastDetailsContext = createContext(null);
+
+export default function Layout({params, children}: {children: React.ReactNode})
 {
-    const [summary, setSummary] = useState('')
-    const [imageSource, setImageSource] = useState('')
-    const [author, setAuthor] = useState('')
-    const [title, setTitle] = useState('')
-
     const podcast = params.podcast;
-    const [podcastDetails, setPodcastDetails] = useState([]);
-    var cachedPodcastInfo: any;
+    const podcastDetailsCacheKey: string = `podcastDetails${podcast}`;
+    const podcastDetailsTimeStampKey: string = `podcastDetails${podcast}TimeStamp`;
+
+    const [podcastDetails, setPodcastDetails] = useState(
+    {
+        artworkUrl600: '',
+        artistName: '',
+        collectionName: '',
+        feedUrl: '',
+        summary: ''
+    });
+
+    const readCachedPodcastDetails = () =>
+    {
+        return readCachedData(podcastDetailsTimeStampKey, podcastDetailsCacheKey);
+    }
 
     useEffect(() => 
     {
-        const cachedPodcastInfoString = localStorage.getItem(`podcast${podcast}`);
-        cachedPodcastInfo = JSON.parse(cachedPodcastInfoString ? cachedPodcastInfoString : '');
-        setSummary(cachedPodcastInfo.summary.label);
+        const cachedPodcastObject: any = readCachedPodcastDetails();
 
-        fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(`https://itunes.apple.com/lookup?id=${podcast}`)}`)
-        .then(response => response.json())
-        .then(data => 
+        if (cachedPodcastObject === null)
         {
-            var jsonContent = JSON.parse(data.contents);
-            if (jsonContent!.resultCount === 1)
+            fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(`https://itunes.apple.com/lookup?id=${podcast}`)}`)
+            .then(response => response.json())
+            .then(data => 
             {
-                setPodcastDetails(jsonContent.results[0]);       
-                setImageSource(cachedPodcastDetails.artworkUrl600);
-                setAuthor(cachedPodcastDetails.artistName);
-                setTitle(cachedPodcastDetails.collectionName?.toUpperCase());           
-            }
-        })
-        .catch(e => console.error(e))
-    });
+                var jsonContent = JSON.parse(data.contents);
+                if (jsonContent!.resultCount === 1)
+                {
+                    const podcastDetails = jsonContent.results[0];
+                    setPodcastDetails(podcastDetails);
 
-    const cachedPodcastDetails: any = useMemo(() => podcastDetails, [podcastDetails]);
+                    localStorage.setItem(podcastDetailsTimeStampKey, JSON.stringify(((new Date()).getTime())));
+                    localStorage.setItem(podcastDetailsCacheKey, JSON.stringify(podcastDetails));
+                }
+            })
+            .catch(e => console.error(e))
+        }
+        else
+        {
+            setPodcastDetails(cachedPodcastObject);
+        }
 
-    return (<div className=" flex flex-row">
-        <div className= " justify-center items-center shadow-md border m-5 mt-20 p-5">
-            <img className=" max-w-300 max-h-300 p-5" src={imageSource}></img>
-            <hr></hr>
-            <h1 className=" text-center font-semibold">{title}</h1>
-            <p  className=" text-center text-gray-400">{"by " + author}</p>
-            <hr></hr>
-            <div>{summary}</div>
+    }, []);
+
+    return (<div className=" flex flex-row p-5">
+        <a className="max-w-xs max-h-xs	" href={`/podcast/${podcast}`}>
+            <div className= " justify-center items-center shadow-md border m-5 p-5">
+                <img className=" max-w-300 max-h-300 p-5" src={podcastDetails?.artworkUrl600}></img>
+                <hr></hr>
+                <div className=" text-left py-5 ">
+                    <h1 className=" font-bold">{podcastDetails?.collectionName?.toUpperCase()}</h1>
+                    <p  className=" text-gray-400 italic">{"by " + podcastDetails?.artistName}</p>
+                </div>
+                <hr></hr>
+                <div className="mt-5">
+                    <div className="font-bold">Description:</div>
+                    <div className="italic text-ellipsis overflow-hidden">{podcastDetails?.summary?.label}</div>
+                </div>
+            </div>
+        </a>
+        <div className="m-5 mx-20 w-full">
+            <PodcastDetailsContext.Provider value={podcastDetails}>
+                {children}
+            </PodcastDetailsContext.Provider>
         </div>
-        <div>{children}</div>
     </div>)
 }

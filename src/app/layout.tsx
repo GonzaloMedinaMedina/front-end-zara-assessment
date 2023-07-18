bc7754b1675c71c0054135022aa8acccbb3d3228
gonzalo <gonzalomedinamedina@gmail.com>
2023-07-18 20:34:01 +0200

    Moving outside hr element and full width property from div container.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,12 +6,12 @@ export default function RootLayout({children,}: {children: React.ReactNode})
     <html lang="en">
       <body className="bg-white">        
         <main className="flex min-h-screen flex-col items-center justify-start p-5">
-          <div className="self-start min-w-full">
+          <div className="self-start">
             <a href="/">
               <h1 className="text-blue-600 text-3xl">Podcaster</h1>
             </a>
-            <hr></hr>
           </div>
+          <hr className="min-w-full"></hr>
           <div className="relative flex flex-col py-5 min-w-full">
             {children}
           </div>
